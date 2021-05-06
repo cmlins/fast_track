6d47c68d8ed0968239ef2b987b51b35537da74bd
Cinthya Moreira Lins <cinthya.lins@pampa.compasso>
2021-05-06 11:33:03 -0300

    implementa método tranfere

--- OO Python3/conta.py
+++ OO Python3/conta.py
@@ -1,26 +1,35 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        print(f'Contruindo objeto ... {self}')
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        print(f'Construindo objeto ... {self}')
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'Saldo é {self.saldo} do titular {self.titular}.')
+        print('Saldo de {} do titular {}'.format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
 
 
-conta = Conta(123, 'Nemo', 100.0, 100.0)
-print(conta.titular)
-conta.extrato()
-conta.saca(15)
-conta.extrato()
-conta.deposita(300)
-conta.extrato()
+
+conta1 = Conta(123, 'Nemo', 100.0, 100.0)
+conta2 = Conta(234, 'Dory', 1000.0, 2000.0)
+conta1.extrato()
+conta1.saca(15)
+conta1.extrato()
+conta1.deposita(300)
+conta2.transfere(300, conta1)
+conta1.extrato()
